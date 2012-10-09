b5534af33d7c26a548533dde69f56c48f636e2f8
jonac <jonas.ba.carlsson@gmail.com>
2012-10-09 16:26:52 +0200

    updated test script to print number of failed and total tested

--- run_tests.rb
+++ run_tests.rb
@@ -1,17 +1,26 @@
 test = Array.new
 input = ""
+i = 0
+j=0
 
 IO.popen("./pollard_run", mode="w+"){|p_io|
-
+	
 	# print arr ,"\n"
 	File.foreach("test_cases.input") { |line|
+		j+=1
 		p_io.write(line)
 		print line
+		
 		while input != "\n" do
 			input = p_io.readline
+			if input.eql? "fail\n"
+				i+=1
+			end
 			print input
 		end
 		
 		input = ""
 	}
+
+	print "failed ", i , " tests off ",j ," cases\n"
 }
