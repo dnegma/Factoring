e4b5b55ca15412a0164c4eaa0516d02b5511d099
jonac <jonas.ba.carlsson@gmail.com>
2012-10-23 16:33:25 +0200

    updated timer to work in ms instead of clock cycles

--- timer.h
+++ timer.h
@@ -1,20 +1,22 @@
 
-#define CUT_OFF_LIMIT_UPPER 250000
-#define CUT_OFF_LIMIT_LOWER 1000
+#define CUT_OFF_LIMIT_UPPER 1000
+#define CUT_OFF_LIMIT_LOWER 20
 
 struct timer{
 	
 	clock_t times;
 	clock_t limit;
 
+	double diffclock(clock_t clock1,clock_t clock2){
+		clock_t diffticks=clock1-clock2;
+		// std::cout <<"Diff: "<< diffticks << " CLOCKS_PER_SEC: "<< CLOCKS_PER_SEC <<std::endl;
+		double diffms=(double)(diffticks*1000)/CLOCKS_PER_SEC;
+		return diffms;
+	}
+
 	void set_limit(clock_t l){
-		if(l > CUT_OFF_LIMIT_UPPER){
-			limit = CUT_OFF_LIMIT_UPPER;
-		}else if(l < CUT_OFF_LIMIT_LOWER){
-			limit = CUT_OFF_LIMIT_LOWER;
-		}else{
-			limit = l;
-		}
+		// l = diffclock(l, times);
+		limit = l;
 	}
 
 	timer(int t):limit(t){
@@ -22,9 +24,9 @@ struct timer{
 	}
 
 	bool should_break(clock_t val2){
-		clock_t ret = val2- times;
+		clock_t ret = diffclock(val2,times);
 		// times = val2;
-		// std::cout << ret << " " << (times) << std::endl;
+		// std::cout << ret << " " << (limit) << std::endl;
 		return ret >= limit;
 	}
 
